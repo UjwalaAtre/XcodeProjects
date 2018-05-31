4fb01443da764d46536dbfa2808edc31724226cf
Ujwala Atre <ujwala.atre@glodom.com>
2018-05-31 14:27:16 +0530

    changed files

---
+++ TestXproject/TestXproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestXproject
+//
+//  Created by Mac1 on 31/05/18.
+//  Copyright © 2018 Mac1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
